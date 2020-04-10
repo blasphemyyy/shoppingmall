0e437d34869e397231ec5c589efa30acedf458dc
blasphemyyy <60035341+blasphemyyy@users.noreply.github.com>
2020-04-11 00:23:01 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -5,9 +5,7 @@ export function request(config){
     const instance=axios.create({
         // baseURL:'http://123.207.32.32:8000/api/wh',
         // baseURL:'http://106.54.54.237:8000/api/wh',
-        baseURL:'http://152.136.185.210:8000/api/h8',
-        // baseURL:'http://123.207.32.32:8000/api/h8',
-        // baseURL:'http://106.54.54.237:8000/api/h8',
+       
         
         timeout:5000
     })
